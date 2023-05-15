2f2e13bd2f1e02ff2d30038b614cc9131bcd2a7e
Joseph LaVia <josephdlavia@gmail.com>
2023-05-15 14:22:07 -0500

    Number guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,37 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = 12;
+            Console.WriteLine("Try to guess my favorite number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput > favNumber)
+            {
+                Console.WriteLine("Wrong guess again!");
+            }
+            else if (favNumber > userInput)
+            {
+                Console.WriteLine("Nice try, guess agian!");
+            }
+            else
+            {
+
+                Console.WriteLine("CORRECT!");
+            }
+
+            
+            
+            
+
+
+
+            
+          
         }
     }
 }
